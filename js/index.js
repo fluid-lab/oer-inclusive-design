4af244ce0345945f2e349f6252b07d523221748d
kathylucc <kathy.hanh.luc@gmail.com>
2017-06-09 15:10:01 -0400

    WOrking mobile navigation. Strange appearnace for navigation on desktop view

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ var oer = oer || {};
 
 	$(document).ready(function(){
 		oer.menuToggleIcon = $("#nav-icon3");
-		oer.menu = $("#hiddenMobileMenu");
+		oer.menu = $("#menu");
 
 		oer.menuStateAssignment(oer.menu, oer.menuToggleIcon);
 
